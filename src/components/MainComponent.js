eff8fbb26da43f28205aae95b024385d6eb9c9b0
Kirill Lopatin <lopatin.kg@gmail.com>
2021-05-09 13:53:28 +0300

    assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,10 +3,16 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-// import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
+
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
 
@@ -27,6 +33,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
             selectedDish: null
         };
     }
@@ -39,15 +48,40 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promote) => promote.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
+
+        const DishWithID = ({match}) => { // { match, component, history }
+            return (
+                <DishDetail
+                    dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishID, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishID === parseInt(match.params.dishID, 10))}
+                />
+            );
+        }
+
+        const AboutPage = () => {
+            return (
+                <About
+                    leaders={this.state.leaders}
+                />
+            );
+        }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishID' component={DishWithID} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' component={AboutPage} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
---
+++ src/shared/comments.js
@@ -0,0 +1,163 @@
+export const COMMENTS = 
+[
+    {
+        id: 0,
+        dishID: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 1,
+        dishID: 0,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 2,
+        dishID: 0,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 3,
+        dishID: 0,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 4,
+        dishID: 0,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 5,
+        dishID: 1,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 6,
+        dishID: 1,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 7,
+        dishID: 1,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 8,
+        dishID: 1,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 9,
+        dishID: 1,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 10,
+        dishID: 2,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 11,
+        dishID: 2,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 12,
+        dishID: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 13,
+        dishID: 2,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 14,
+        dishID: 2,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 15,
+        dishID: 3,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 16,
+        dishID: 3,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 17,
+        dishID: 3,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 18,
+        dishID: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 19,
+        dishID: 3,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    }
+];
