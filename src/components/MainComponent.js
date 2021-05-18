40b4e5a96593db647a2ca767a11208ecdc108702
Kirill Lopatin <lopatin.kg@gmail.com>
2021-05-18 22:31:28 +0300

    redux thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,7 @@ import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
 import About from './AboutComponent';
 
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -22,7 +22,8 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    addComment: (dishID, rating, author, comment) => dispatch(addComment(dishID, rating, author, comment))
+    addComment: (dishID, rating, author, comment) => dispatch(addComment(dishID, rating, author, comment)),
+    fetchDishes: () => {dispatch(fetchDishes())}
 })
 
 class Main extends Component {
@@ -40,6 +41,10 @@ class Main extends Component {
     // getSnapshotBeforeUpdate()
     // componentDidUpdate()
 
+    componentDidMount() {
+        this.props.fetchDishes();
+    }
+
     onDishSelect(dishID) {
         this.setState({ selectedDish: dishID });
     }
@@ -49,7 +54,9 @@ class Main extends Component {
         const HomePage = () => {
             return (
                 <Home
-                    dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
                     promotion={this.props.promotions.filter((promote) => promote.featured)[0]}
                     leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
@@ -59,7 +66,9 @@ class Main extends Component {
         const DishWithID = ({match}) => { // { match, component, history }
             return (
                 <DishDetail
-                    dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishID, 10))[0]}
+                    dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishID, 10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
                     comments={this.props.comments.filter((comment) => comment.dishID === parseInt(match.params.dishID, 10))}
                     addComment={this.props.addComment}
                 />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, CardImg, CardTitle, CardImgOverlay, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+import { Loading } from './LoadingComponent';
+
 
 const RenderMenuItem = ({dish}) => {
     return (
@@ -17,7 +19,7 @@ const RenderMenuItem = ({dish}) => {
 }
 
 const Menu = (props) => {
-    const jsx_menu = props.dishes.map((dish) => {
+    const jsx_menu = props.dishes.dishes.map((dish) => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
                 <RenderMenuItem dish={dish} />
@@ -25,23 +27,42 @@ const Menu = (props) => {
         );
     });
 
-    return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Menu</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Menu</h3>
-                    <hr />
+    if (props.dishes.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
                 </div>
             </div>
-            <div className="row">
-                {jsx_menu}
+        );
+    }
+    else if (props.dishes.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.dishes.errMess}</h4>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    {jsx_menu}
+                </div>
+            </div>
+        );
 }
         
 
