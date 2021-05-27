a04df8493c967af96610f82f7a732769656c5397
Kirill Lopatin <lopatin.kg@gmail.com>
2021-05-27 22:59:58 +0300

    fix

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -58,8 +58,8 @@ function Home(props) {
               <div className="col-12 col-md m-1">
                   <RenderCard
                     item={props.leader}
-                    isLoading={props.leaderLoading}
-                    errMess={props.leaderErrMess}
+                    isLoading={props.leadersLoading}
+                    errMess={props.leadersErrMess}
                   />
               </div>
           </div>
